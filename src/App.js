3dc923ce84dae696e9521a56192301e89e4becf1
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-27 14:57:36 -0400

    created 3 useState hooks: one for loading (default value is a boolean), one for jobs (default value is an empty array), and last is for value (default value is set for zero for first item in an array)

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import { FaAngleDoubleRight } from 'react-icons/fa';
 const url = 'https://course-api.netlify.app/api/react-tabs-project';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  return <h2>tabs project setup</h2>;
 }
 
 export default App;
+
+// 1) need to setup a fetch function to fetch the data
+// 2) create 3 useState hooks: one for loading which default value will bea boolean; one for jobs which default value will be an empty array; last hook will be for a value property which its default value will be zero (because of the array we will fetch and looking for the first item)
